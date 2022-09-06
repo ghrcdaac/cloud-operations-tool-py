658c7345dbd1a4937c3ed95c5d88e294521ce586
Michael Hall <mlh0079@uah.edu>
2022-09-06 14:38:08 -0500

    - Comment cleanup.

--- pylot/plugins/cumulus_api/main.py
+++ pylot/plugins/cumulus_api/main.py
@@ -63,12 +63,12 @@ class CumulusApi:
     def get_generic_records(self, record_type, **kwargs):
         """
 
-        :param record_type: hello
-        :type record_type:a
-        :param kwargs:g
-        :type kwargs:g
-        :return:h
-        :rtype:h
+        :param record_type:
+        :type record_type:
+        :param kwargs:
+        :type kwargs:
+        :return:
+        :rtype:
         """
         return self.__crud_records(record_type=record_type, verb='get', **kwargs)
 
