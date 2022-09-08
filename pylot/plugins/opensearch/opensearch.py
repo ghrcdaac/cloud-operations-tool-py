1566fb7ce46557c0356122970a28022c10a76680
Michael Hall <mlh0079@uah.edu>
2022-09-08 17:21:06 -0500

    - Made the record type a positional argument.

--- pylot/plugins/opensearch/opensearch.py
+++ pylot/plugins/opensearch/opensearch.py
@@ -5,28 +5,26 @@ import boto3
 
 
 class OpenSearch:
-    def __init__(self):
-        pass
-
     @staticmethod
-    def __read_json_file(filename):
+    def read_json_file(filename):
         with open(filename, 'r', encoding='utf-8') as file:
             data = json.loads(file.read())
 
         return data
 
-    def query_opensearch(self, query_data, record_type, limit=100):
+    @staticmethod
+    def query_opensearch(query_data, record_type, limit=100):
         lambda_arn = os.getenv('OPENSEARCH_LAMBDA_ARN')
         if not lambda_arn:
             raise Exception('The ARN for the OpenSearch lambda is not defined. Provide it as an environment variable.')
 
         # Invoke OpenSearch lambda
-        data = self.__read_json_file(query_data)
         payload = {
-            'query': data,
+            'query': query_data,
             'record_type': str(record_type).rstrip('s'),
             'limit': limit
         }
+
         print('Invoking OpenSearch lambda...')
         client = boto3.client('lambda')
         rsp = client.invoke(
@@ -34,8 +32,9 @@ class OpenSearch:
             Payload=json.dumps(payload).encode('utf-8')
         )
         if rsp.get('StatusCode') != 200:
-            raise Exception(f'The OpenSearch lambda failed. Check the Cloudwatch logs for '
-                            f'{os.getenv("OPENSEARCH_LAMBDA_ARN")}')
+            raise Exception(
+                f'The OpenSearch lambda failed. Check the Cloudwatch logs for {os.getenv("OPENSEARCH_LAMBDA_ARN")}'
+            )
 
         # Download results from S3
         print('Downloading query results...')
@@ -55,16 +54,40 @@ class OpenSearch:
 
 
 def return_parser(subparsers):
-    subparser = subparsers.add_parser('opensearch',
-                                      help='This plugin is used to submit queries directly to OpenSearch '
-                                           'bypassing the cumulus API.',
-                                      description='Submit queries to opensearch')
-    subparser.add_argument('-d', '--data',  help='The name of a file containing a json query',
-                           metavar='<filename>.json',
-                           default='query.json',
-                           required=True)
+    subparser = subparsers.add_parser(
+        'opensearch',
+        help='This plugin is used to submit queries directly to OpenSearch bypassing the cumulus API.',
+        description='Submit queries to opensearch'
+    )
+    subparser.add_argument(
+        'record_type',
+        help='The OpenSearch record type to be queried. granules/providers/collections etc.',
+        metavar='record_type'
+    )
+    subparser.add_argument(
+        '-l', '--limit',
+        help='Limit the number of records returned from OpenSearch',
+        metavar='',
+        default=100
+    )
+    group = subparser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        '-d', '--data',
+        help='The name of a file containing a json query: <filename>.json',
+        metavar=''
+    )
+    group.add_argument(
+        '-q', '--query',
+        help='A JSON query string using the OpenSearch DSL: '
+             'https://opensearch.org/docs/latest/opensearch/query-dsl/index/ '
+             'Example: \'{"query": {"term": {"collectionId": "goesimpacts___1"}}}\'',
+        metavar=''
+    )
 
 
-def main(args):
-    print(f'opensearch_main: {args}')
-    return 'success'
+def main(record_type, query=None, data=None, **kwargs):
+    query = json.loads(query) if query else OpenSearch.read_json_file(data)
+    res = OpenSearch.query_opensearch(query_data=query, record_type=record_type, **kwargs)
+    print(res)
+
+    return 0
